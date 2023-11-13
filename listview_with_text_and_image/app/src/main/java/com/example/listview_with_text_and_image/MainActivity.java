9e50c93de9261d83f6712f773dc7707b976f686b
997heng <997heng@gamil.com>
2023-11-13 23:22:29 +0800

    hw6

--- listview_with_text_and_image/app/src/main/java/com/example/listview_with_text_and_image/MainActivity.java
+++ listview_with_text_and_image/app/src/main/java/com/example/listview_with_text_and_image/MainActivity.java
@@ -2,11 +2,14 @@ package com.example.listview_with_text_and_image;
 
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import android.content.Context;
 import android.content.DialogInterface;
 import android.os.Bundle;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
@@ -14,6 +17,7 @@ import android.widget.BaseAdapter;
 import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.List;
 
@@ -29,6 +33,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setTitle("");
+        getSupportActionBar().setLogo(R.drawable.t1);
+        getSupportActionBar().setDisplayUseLogoEnabled(true);
+
+
 
         lst = (ListView)findViewById(R.id.listView);
         final MyAdapter adapter = new MyAdapter(this);
@@ -47,6 +58,49 @@ public class MainActivity extends AppCompatActivity {
                ).show();
             }
         });
+    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.m1:
+                new AlertDialog.Builder(MainActivity.this).setTitle("trivago").setMessage("trivago 飯店搜尋讓用戶輕鬆點選幾下，就可以從數百個訂房網站及超過 190 個國家的 500 多萬家飯店等類型住宿中，找到最優惠的價格。我們每年幫助數百萬名旅客比較飯店等住宿價格。您可以在 trivago 上找到週末短程旅遊的資訊，如東京或大阪，簡單迅速就能找到適合您的飯店。您同樣也可以找適合旅行一週或以上的城市，如紐約及其周邊的飯店。").setPositiveButton(
+                        "OK",new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialogInterface, int i) {
+                            }
+                        }
+                ).show();
+                break;
+            case R.id.m2:
+                new AlertDialog.Builder(MainActivity.this).setTitle("how to get there" ).setMessage("坐車").setPositiveButton(
+                        "OK",new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialogInterface, int i) {
+                            }
+                        }
+                ).show();
+                break;
+            case R.id.m3:
+                new AlertDialog.Builder(MainActivity.this).setTitle("reservation").setMessage("很貴").setPositiveButton(
+                        "OK",new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialogInterface, int i) {
+                            }
+                        }
+                ).show();
+                break;
+            case R.id.m4:
+                finish();
+                break;
+        }
+        return super.onOptionsItemSelected(item);
     }
 
     //////////////////////////
